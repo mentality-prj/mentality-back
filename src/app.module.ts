f4445406a0fa16f0c177667a247f381c23051c87
Taras Gez <taraspixel@gmail.com>
2024-10-29 20:54:51 +0100

    fix lint errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesController } from './articles/articles.controller';
 import { OpenaiService } from './openai/openai.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot()],

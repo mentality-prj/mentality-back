1130339f6469280d326f14ed4fe80f5cbf34b706
Taras Gez <taraspixel@gmail.com>
2024-10-29 22:28:33 +0100

    add mock test for user

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,13 +1,26 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { User } from './schemas/user.schema';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUserModel = {
+    find: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockUserModel,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);

f466f4c746dade877ec38086bdee175c64430101
Taras Gez <taraspixel@gmail.com>
2024-11-05 14:38:39 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  // Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription(
+      'Description of the documentation for the Mentality project',
+    )
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Swagger documentation on the root route
+  SwaggerModule.setup('', app, document);
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

329a85259cd76dc39c047ffd9e6d2a745378e983
PetrashchukIM <petrashchukim@gmail.com>
2025-03-25 13:45:07 +0200

    style: added coments

--- src/affirmations/affirmations.controller.ts
+++ src/affirmations/affirmations.controller.ts
@@ -15,43 +15,37 @@ import { AffirmationsService } from './affirmations.service';
 import { Affirmation } from './schemas/affirmation.schema';
 
 @Controller('affirmations')
-// Controller for handling routes related to affirmations
 export class AffirmationsController {
   constructor(private readonly affirmationsService: AffirmationsService) {}
 
+  // Generate and save an affirmation
   @Post('generate')
-  // Endpoint to generate and save a new affirmation
   async generateAffirmation() {
     return this.affirmationsService.generateAndSaveAffirmation();
   }
 
+  // Get affirmations with pagination
   @Get()
-  // Endpoint to fetch affirmations with pagination
   async getManyWithPagination(
-    // Extract 'page' query parameter, default to PAGE constant if not provided
     @Query('page', new ParseIntPipe()) page = PAGE,
-    // Extract 'limit' query parameter, default to LIMIT constant if not provided
     @Query('limit', new ParseIntPipe()) limit = LIMIT,
   ): Promise<{ data: Affirmation[]; total: number }> {
-    // Fetch paginated affirmations and total count from the service
     const { data, total } =
+      // Get affirmations with pagination
       await this.affirmationsService.getManyAffirmationsWithPagination(
         page,
         limit,
       );
-    // Return the data and total count
     return { data, total };
   }
 
+  // Update an affirmation's published status
   @Patch(':id/publish')
-  // Endpoint to update the 'isPublished' status of an affirmation
   async updateAffirmation(
-    // Extract 'id' parameter from the route
     @Param('id') id: string,
-    // Extract 'isPublished' field from the request body
     @Body() body: { isPublished: boolean },
   ): Promise<Affirmation> {
-    // Update the affirmation's 'isPublished' status using the service
+    // Update the affirmation's published status
     return this.affirmationsService.updateAffirmation(id, body.isPublished);
   }
 }

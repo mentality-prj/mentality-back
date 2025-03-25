327d8bd9b2e313d0323067b6e9328a99f073a384
PetrashchukIM <petrashchukim@gmail.com>
2025-03-25 13:45:07 +0200

    style: added coments

--- src/affirmations/affirmations.controller.ts
+++ src/affirmations/affirmations.controller.ts
@@ -15,13 +15,11 @@ import { AffirmationsService } from './affirmations.service';
 import { Affirmation } from './schemas/affirmation.schema';
 
 @Controller('affirmations')
-// Controller for handling routes related to affirmations
 export class AffirmationsController {
   constructor(private readonly affirmationsService: AffirmationsService) {}
 
   // Generate and save an affirmation
   @Post('generate')
-  // Endpoint to generate and save a new affirmation
   async generateAffirmation() {
     return this.affirmationsService.generateAndSaveAffirmation();
   }
@@ -37,20 +35,16 @@ export class AffirmationsController {
   }
 
   @Get()
-  // Endpoint to fetch affirmations with pagination
   async getManyWithPagination(
-    // Extract 'page' query parameter, default to PAGE constant if not provided
     @Query('page', new ParseIntPipe()) page = PAGE,
-    // Extract 'limit' query parameter, default to LIMIT constant if not provided
     @Query('limit', new ParseIntPipe()) limit = LIMIT,
   ): Promise<{ data: Affirmation[]; total: number }> {
-    // Fetch paginated affirmations and total count from the service
     const { data, total } =
+      // Get affirmations with pagination
       await this.affirmationsService.getManyAffirmationsWithPagination(
         page,
         limit,
       );
-    // Return the data and total count
     return { data, total };
   }
 }
--- src/affirmations/affirmations.service.ts
+++ src/affirmations/affirmations.service.ts
@@ -10,46 +10,44 @@ import { Affirmation } from './schemas/affirmation.schema';
 @Injectable()
 export class AffirmationsService {
   private openAIService: MockOpenAIService;
+  // Inject the Affirmation model
   constructor(
     @InjectModel(Affirmation.name) private affirmationModel: Model<Affirmation>,
   ) {
-    // Initialize the mock OpenAI service
     this.openAIService = new MockOpenAIService();
   }
 
-  // Generate a single affirmation text using the mock OpenAI service
+  // Generate an affirmation text
   async generateAffirmationText(): Promise<string> {
     const texts = await this.openAIService.generateAffirmationText();
-    // Return the first text if available, otherwise return an empty string
+    // Return the first text if it exists
     return Array.isArray(texts) && texts.length > 0 ? texts[0] : '';
   }
 
-  // Generate an image based on a given prompt using the mock OpenAI service
+  // Generate an image
   async generateImage(_prompt: string): Promise<string> {
     const images = await this.openAIService.generateImage(_prompt);
-    // Return the first image URL if available, otherwise return an empty string
+    // Return the first image if it exists
     return Array.isArray(images) && images.length > 0 ? images[0] : '';
   }
 
-  // Simulate uploading an image to Cloudinary and return the image URL
+  // Upload to Cloudinary
   async uploadToCloudinary(imageUrl: string): Promise<string> {
     return imageUrl;
   }
 
-  // Generate an affirmation (text and image), save it to the database, and return the saved affirmation
+  // Generate and save an affirmation
   async generateAndSaveAffirmation(): Promise<Affirmation> {
-    // Generate affirmation text
+    // Generate an affirmation value
     const text = await this.generateAffirmationText();
-    // Generate an image based on the text
     const imageUrl = await this.generateImage(text);
-    // Simulate uploading the image to Cloudinary
     const cloudinaryUrl = await this.uploadToCloudinary(imageUrl);
 
-    // Create a new affirmation document in the database
+    // Create a new affirmation
     const newAffirmation = await this.affirmationModel.create({
       text,
       imageUrl: cloudinaryUrl,
-      isPublished: false, // Default to not published
+      isPublished: false,
     });
 
     return newAffirmation;
@@ -60,9 +58,7 @@ export class AffirmationsService {
     page: number = PAGE,
     limit: number = LIMIT,
   ): Promise<{ data: Affirmation[]; total: number }> {
-    // Calculate the number of documents to skip based on the page and limit
     const skip = (page - 1) * limit;
-    // Get affirmations while skipping and limiting the results
 
     // Fetch affirmations sorted by creation date in descending order
     const affirmations = await this.affirmationModel
@@ -71,24 +67,21 @@ export class AffirmationsService {
       .skip(skip)
       .limit(limit)
       .exec();
-    //  Get the total count of affirmations
 
     // Count the total number of affirmations in the database
     const total = await this.affirmationModel.countDocuments().exec();
-
     return { data: affirmations as Affirmation[], total };
   }
 
-  // Update the publication status of an affirmation by its ID
+  //  Update an affirmation
   async updateAffirmation(
     id: string,
     isPublished: boolean,
   ): Promise<Affirmation> {
-    // Find the affirmation by ID and update its publication status
+    // Update the affirmation's published
     const updatedAffirmation = await this.affirmationModel
       .findByIdAndUpdate(id, { isPublished }, { new: true })
       .exec();
-
     // Throw an error if the affirmation is not found
     if (!updatedAffirmation) {
       throw new Error('Affirmation not found');
